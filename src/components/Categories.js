84e37a6ed2f94c2791cdf8fef19023794dfcf3eb
Mhamed Meguitif <meguitifmhamed3@gmail.com>
2022-12-23 21:31:51 +0100

    menu complete

---
+++ src/components/Categories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Categories = ({filterItems  , categories}) => {
+
+  return <div className='btn-container'>
+    {categories.map((item , index)=>{
+        return  <button type='button' className='filter-btn'  key={index} onClick={()=>filterItems(item)}>
+        {item}</button>
+    })}
+   
+    
+  </div>;
+};
+
+export default Categories;
